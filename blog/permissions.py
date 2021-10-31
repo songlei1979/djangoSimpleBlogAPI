2c3f64a129e8e9be33afe58ff0243b144b3caf01
Lei <gabriel_sl19798@hotmail.com>
2021-11-01 02:03:39 +1300

    testing user permission

--- blog/permissions.py
+++ blog/permissions.py
@@ -25,7 +25,7 @@ class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if view.action == 'list':
-            return request.user.is_authenticated() and request.user.is_admin
+            return request.user.is_authenticated and request.user.is_admin
         elif view.action == 'create':
             return True
         elif view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
